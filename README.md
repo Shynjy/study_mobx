ad8665aa7582b033e4ac0bb266d9c30f888b3c7a
Shynjy <furia.designer@gmail.com>
2021-01-06 18:31:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Tutorial MobX
+
+Documentação de estudo rápido para Mobx
+
+
+## Método apenas com as dependências Mobx e flutter_mobx
+
+### Dentro do Controller
+
+```dart
+Action increment;
+
+Construtor() {
+    increment = Action(_increment);
+}
+
+Observable<int> _count = Observable(0);
+
+int get count => _count.value;
+
+void _increment() {
+    _count.value++;
+}
+```
+
+### Usando no widget
+
+```dart
+var counter = Construtor();
+
+Observer( builder: (_) { return Text('${counter.count}');},),
+```
+
+## Método com as dev-dependências mobx_codegen e build_runner
+Código para gerar os arquivos apenas uma vez: `flutter packages pub run build_runner build`
+Código para gerar os arquivos sempre que atualizar: `flutter packages pub run build_runner watch`
+
+Passo 1:
+> crie um arquivo para gerenciar os estados com o mobx
+
+```dart
+part 'controller_mobx.g.dart';
+
+class ControllerMobx = _ControllerMobx with _$ControllerMobx;
+
+abstract class _ControllerMobx with Store {
+@observable
+int count = 0;
+
+@action
+void increment() {
+    count++;
+}
+}
+```
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+// Tipos
+import './models/controller_mobx.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MobX Tutorial',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var counter = ControllerMobx();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MobX Tutorial'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(
+              builder: (_) {
+                return Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
