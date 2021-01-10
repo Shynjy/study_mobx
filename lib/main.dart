ffc0ef765810538fbf2eb98f1727c7b54ad57fb8
Shynjy <furia.designer@gmail.com>
2021-01-10 09:14:56 -0300

    update app

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,21 @@
 import 'package:flutter/material.dart';
+import './screens/login_screen.dart';
 
-import 'package:flutter_mobx/flutter_mobx.dart';
-
-// Tipos
-import './models/controller_mobx.dart';
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'MobX Tutorial',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.deepPurpleAccent,
+        cursorColor: Colors.deepPurpleAccent,
+        scaffoldBackgroundColor: Colors.deepPurpleAccent,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var counter = ControllerMobx();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MobX Tutorial'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Observer(
-              builder: (_) {
-                return Text(
-                  '${counter.count}',
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/stores/login_store.dart
@@ -0,0 +1,39 @@
+import 'package:mobx/mobx.dart';
+
+part 'login_store.g.dart';
+
+class LoginStore = _LoginStore with _$LoginStore;
+
+abstract class _LoginStore with Store {
+  _LoginStore() {
+    autorun(
+      (_) {
+        //Fica escutando toda alteração de um observable
+        print(isFormValid);
+      },
+    );
+  }
+
+  @observable
+  String email = '';
+
+  @action
+  void setEmail(String valeu) => email = valeu;
+
+  @observable
+  String password = '';
+
+  @action
+  void setPassword(String valeu) => password = valeu;
+
+  @computed
+  bool get isEmailValid => 
+  RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+      .hasMatch(email);
+
+  @computed
+  bool get isPasswordValid => password.length >= 6;
+
+  @computed
+  bool get isFormValid => isEmailValid && isPasswordValid;
+}
